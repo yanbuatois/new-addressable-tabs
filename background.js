940101f2a1e361dbbd26b39a89640ef1a1f8aa25
Yan Buatois <yan.buatois@mediapart.fr>
2026-07-15 13:42:46 +0200

    ✨ move to last tab when using Ctrl+9

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
+const TAB_INDEX_REGEX = /tab(\d+)/;
+const LAST_TAB_COMMAND_NAME = 'lastTab';
+
 browser.commands.onCommand.addListener(name => {
-  const index = parseInt(/tab(\d+)/.exec(name)[1], 10);
-  browser.tabs.query({currentWindow: true, index}).then(tabs => {
-    if (tabs.length > 0)
-      browser.tabs.update(tabs[0].id, {active: true});
-  })
+  if (name === LAST_TAB_COMMAND_NAME) {
+    browser.tabs.query({currentWindow: true}).then(tabs => {
+      if (tabs.length > 0) {
+        browser.tabs.update(tabs.at(-1).id, { active: true })
+      }
+    });
+  } else {
+    const index = parseInt(TAB_INDEX_REGEX.exec(name)[1], 10);
+    browser.tabs.query({currentWindow: true, index}).then(tabs => {
+      if (tabs.length > 0)
+        browser.tabs.update(tabs[0].id, { active: true });
+    })
+  }
 })
